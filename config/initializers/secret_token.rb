17aa6a7fef6b19355f269a73d0fba228d26512b0
Omar Paro <omar.paro@googlemail.com>
2011-02-06 17:21:55 +0000

    commit oneuro

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oneuro::Application.config.secret_token = '4704732657cfe69aeb8f66cf7277eedb07e7a2cf18d1db98f61103f018c640564f8cc78aa477efd197dd6974f7497e9816d8ec99b6174985b9a6e2a200c29372'
